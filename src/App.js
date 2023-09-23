0072f68dcb93e3a068678ac300129644da4989da
Thiru-pathi <94216693+Thiru-pathi@users.noreply.github.com>
2023-09-23 17:37:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>Azure</code> CI-CD projects.
+           <code>Azure to github</code> CI-CD projects.
         </p>
         <a
           className="App-link"
